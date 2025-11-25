dd921727b89a4adea34a78ce681ab21e3e7f061e
jackson <jackson.primodev@gmail.com>
2025-11-24 21:34:24 -0300

    add exception handler in controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,11 +1,11 @@
 class AuthController < ApplicationController
   def signin
     result = ::Auth::Signin.new(params).call
-    render_result result
+    render json: result
   end
 
   def signup
     result = ::Auth::Signup.new(params).call
-    render_result result
+    render json: result
   end
 end
---
+++ lib/custom_exception.rb
@@ -0,0 +1,8 @@
+class CustomException < Exception
+  attr_reader :code
+
+  def initialize(message, error_code=500)
+    super(message)
+    @code = error_code
+  end
+end
